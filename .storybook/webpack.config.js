9b84246c74013cf2911a952a7fe1b4c733f0bcc5
Diego Jara Palomino <saintplay96@gmail.com>
2019-05-08 21:05:26 -0500

    adding imports, fix import path, remove plugin

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require("webpack");
 module.exports = ({ config }) => {
   const customPlugins = [
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new (require("rewiremock/webpack/plugin"))()
   ];
 
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import rewiremock from "rewiremock/webpack";
 
-const ComponentExample = rewiremock.proxy("./Lobby", {
-  "./lib": {
-    COUNT: 100
+import "./lib";
+import "./ComponentExample";
+
+const ComponentExample = rewiremock.proxy(
+  () => require("./ComponentExample"),
+  () => {
+    rewiremock(() => require("./lib")).with({
+      COUNT: 100
+    });
   }
-});
+).default;
 
 storiesOf("ComponentExample", module).add("with mocked data", () => (
   <ComponentExample />
